9eafd37caf70ff56ba9c54b212efcd90be3285db
novaariyanto <bajangsampoerna@gmail.com>
2022-08-12 09:27:12 +0700

    initial files

---
+++ example.php
@@ -0,0 +1,13 @@
+<?php
+include("./src/whatsapp-api/whatsva.php");
+
+$whatsva = new Whatsva();
+
+$instance_key = "IiCVefn6gJG0";
+$jid = "62895361034833";
+$message = "hello";
+
+
+$sendMessage = $whatsva->sendMessageText($instance_key,$jid,$message);
+
+print_r($sendMessage);
